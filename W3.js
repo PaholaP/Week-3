6d70380662c19574612506550348a47e1f5719bc
Pahola <102555771+PaholaP@users.noreply.github.com>
2022-04-20 23:53:52 -0700

    Update W3.js

--- W3.js
+++ W3.js
@@ -35,11 +35,11 @@ console.log(sum / names.length);
 var con = names[0];
 for(i=1; i < names.length; i++)
 {
-    con = con.concat("",names[i]);
+    con = con.concat(" " , names[i]);
 }
 console.log("Concatenated names :",con);
-// How do you access the last element of any array?
 
+// How do you access the last element of any array?
 //Answer: Storing lengths of each names in nameLengths Array
 //Ex:
 
@@ -56,7 +56,7 @@ for (i = 0; i < nameLengths.length; i++) {
  sum = sum + nameLengths[i];
 }
 console.log(sum);
-console.log(/n/n/n);
+console.log("/n/n/n");
 
 // Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
 // For example:
@@ -76,16 +76,63 @@ console.log('namesLengths: ', namesLengths);
 
 let totalLengths = 0;
 
-for(var i = 0; i <nameLengths.length; i++)
+for(var i = 0; i < nameLengths.length; i++)
 totalLengths += nameLengths [i];
 
 console.log('Sum of all elements: ', totalLengths);
     
 // Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).
 
+function concat_n_times(word, n) {
+    var con = word;
+    for (i = 1; i <= n; i++) { 
+        con = con.concat(word);
+    } return con;
+}
+
+console.log(concat_n_times("Hello ", 3));
+console.log("\n\n\n");
+
 // Write a function that takes two parameters, firstName and lastName, and returns a full name (the full name should be the first and the last name separated by a space).
 
+function fullName(firstName,lastName) {
+    return firstName.concat("", lastName) 
+    }
+console.log("Full Name: ",fullName("Lola " ,"Palacios"));
+
+console.log("\n\n\n")
+
+console.log(concat_n_times("Hello ", 3));
+console.log("\n\n\n");
+
+// Write a function that takes two parameters, firstName and lastName, and returns a full name (the full name should be the first and the last name separated by a space).
+
+function fullName(firstName,lastName) {
+    return firstName.concat("", lastName) 
+    }
+console.log("Full Name: ",fullName("Lola " ,"Palacios"));
+
+console.log("\n\n\n");
 // Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
+
+function isTotalGreaterThan100(arr) {
+    sum = 0;
+    for (i =0; i <arr.length; i++) {
+        sum = sum + arr[i];
+        if(sum > 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+function compare_average(arr1,arr2) {
+    if (average (arr) > average(arr2)) {
+
+    }
+}
+
 
 // Write a function that takes an array of numbers and returns the average of all the elements in the array.
 
@@ -97,16 +144,25 @@ function average(arr) {
     return (sum /arr.length);
 }
 
+
 // Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 
 function compare_average(arr1,arr2) {
- if(average(arr1) >average(arr2))
-{
+ if (average(arr1) > average(arr2)) {
     return true;
-} else { return false;
+} else  {
+     return false;
 
 }
 }
+
+var myArr = [12, 24, 36, 48, 60 ]
+var myArr2 = [24, 36, 12]
+
+console.log(isTotalGreaterThan100(myArr));
+console.log(average(myArr));
+console.log(compare_average(myArr,myArr2));
+console.log("\n\n\n");
 
 // Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 
@@ -126,4 +182,4 @@ function alphabet_order(str)
   {
 return str.split('').sort().join('');
   }
-console.log(alphabet_order("baby"));
+console.log(alphabet_order("baby")); 
